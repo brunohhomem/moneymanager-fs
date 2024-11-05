56dfee6b06e49f0c59a7c9cdaa5dab5f7a246290
Bruno H. Homem <brunohhomem@gmail.com>
2024-11-05 07:53:19 -0300

    feat: add `ActivityGateway`

---
+++ money-manager-back/app/src/main/java/moneymanager/domain/gateway/ActivityGateway.java
@@ -0,0 +1,15 @@
+package moneymanager.domain.gateway;
+
+import moneymanager.domain.activity.Activity;
+
+import java.util.List;
+
+public interface ActivityGateway {
+
+    public void create(final Activity anActivity);
+
+    public void delete(final String anId);
+
+    public List<Activity> findAll(final Activity anActivity);
+
+}
