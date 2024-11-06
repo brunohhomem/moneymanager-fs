af0a52006c656158de4ee1b131a31c4eae92237a
Bruno H. Homem <brunohhomem@gmail.com>
2024-11-06 11:59:46 -0300

    feat: added `ActivityService`

---
+++ money-manager-back/app/src/main/java/moneymanager/domain/service/ActivityService.java
@@ -0,0 +1,26 @@
+package moneymanager.domain.service;
+
+import moneymanager.domain.service.dto.InsertActivityInputDTO;
+import moneymanager.domain.service.dto.InsertActivityOutputDTO;
+import moneymanager.domain.service.dto.ListActivitiesOutputDTO;
+
+import java.util.List;
+
+public class ActivityService {
+
+    public InsertActivityOutputDTO insertActivity(final InsertActivityInputDTO anInput) {
+
+    }
+
+    public void removeActivity(final String anId) {
+
+    }
+
+    public List<ListActivitiesOutputDTO> listActivities() {
+
+    }
+
+    public float calculateBalance() {
+
+    }
+}
