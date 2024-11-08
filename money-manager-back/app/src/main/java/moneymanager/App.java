9d4ff8966e8b763c7ba80fb83728ab4f861a04d9
Bruno H. Homem <brunohhomem@gmail.com>
2024-11-08 09:59:06 -0300

    config: add `WebServerConfiguration`

--- money-manager-back/app/src/main/java/moneymanager/App.java
+++ money-manager-back/app/src/main/java/moneymanager/App.java
@@ -3,8 +3,23 @@
  */
 package moneymanager;
 
+import moneymanager.configuration.WebServerConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        LOG.info("Starting moneymanager... ");
+
+        SpringApplication.run(WebServerConfiguration.class, args);
+
+        LOG.info("Ending of starting moneymanager... ");
     }
 }
---
+++ money-manager-back/app/src/main/java/moneymanager/configuration/WebServerConfiguration.java
@@ -0,0 +1,9 @@
+package moneymanager.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("moneymanager")
+public class WebServerConfiguration {
+}
