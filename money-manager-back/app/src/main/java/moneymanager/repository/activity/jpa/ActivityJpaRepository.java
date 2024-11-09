72a12b58ccf59b883e73c9185e2c353d26cf3744
Bruno H. Homem <brunohhomem@gmail.com>
2024-11-09 11:28:55 -0300

    config: add jpa and ActivityJpaRepository

---
+++ money-manager-back/app/src/main/java/moneymanager/repository/activity/jpa/ActivityJpaRepository.java
@@ -0,0 +1,6 @@
+package moneymanager.repository.activity.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityJpaRepository extends JpaRepository<ActivityJpaEntity, String> {
+}
